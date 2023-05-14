4fe2fe30d59ab18eebedf95ab77d90092a05190e
Egor Kuznetsov <egor-kuznetsov2004@mail.ru>
2023-05-14 13:38:29 +0300

    ADD: tg_bot example

---
+++ lib/tg_bot/README.md
@@ -0,0 +1,9 @@
+### Установка библиотек
+1. Установка и компиляция telegram-api для С++
+```  
+sudo apt-get install libboost-all-dev libssl-dev 
+git clone https://github.com/reo7sp/tgbot-cpp.git ./lib/tg_bot/lib/tg_bot_api
+cd ./lib/tg_bot/lib/tg_bot_api
+cmake .  
+make -j4
+```
--- lib/tg_bot/src/bot.cpp
+++ lib/tg_bot/src/bot.cpp
@@ -1,5 +1,23 @@
-#include "bot.hpp"
+#include <iostream>
+#include <tgbot/tgbot.h>
 
 void bot(){
-    std::cout << "Tg bot printing" << std::endl;
+    TgBot::Bot bot("6216802370:AAHtsAHiNSVm4PZv2r1YZHdQu5ap63LF8kE");
+
+    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
+        bot.getApi().sendMessage(message->chat->id, "Hello, world!");
+    });
+     bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
+        std::cout << "User wrote " << message->text << std::endl;
+    });
+
+    try {
+        std::cout << "Bot username: " << bot.getApi().getMe()->username << std::endl;
+        TgBot::TgLongPoll longPoll(bot);
+        while (true) {
+            longPoll.start();
+        }
+    } catch (TgBot::TgException& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+    }
 }
