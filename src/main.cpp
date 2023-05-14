3cfb44a2c8495579c0adc5d0d9e2233c9249aa68
kotobopot <borisov_vk@mail.ru>
2023-05-14 15:42:37 +0300

    main.cpp cv usage addded

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,12 @@
 #include "server.h"
 #include "routes.h"
 
+#include "VideoProcessor.h"
+
 int main(int argc, char* argv[]) {
+  VideoProcessor vp = VideoProcessor(0);
+  std::cout<<"Current people amount on web camera: " <<vp.getQueuePeopleAmount()<<std::endl;
+  
   auto rt = getRouter();
   Server tst(*rt);
 
