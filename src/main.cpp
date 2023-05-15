1972eb549c44aca6a81bbde951415f2b18a04375
TimofeyTst <yarik.mist@gmail.com>
2023-05-15 16:36:54 +0300

    Сделал запись в базу данных

--- src/main.cpp
+++ src/main.cpp
@@ -6,29 +6,34 @@
 #include "video_processors.h"
 
 int main(int argc, char* argv[]) {
-  // Создаем первый поток для цикла while(true)
-  std::thread video_thread([]() {
-    while (true) {
-      auto vid_processor =
-          getVideoProcessor("../static/video_examples/ex1.mp4", 0.2);
-
-      for (int i = 0; i < 28; ++i) {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        std::cout << i << " step. Current people amount from videosource: "
-                  << vid_processor->getQueuePeopleAmount()[0]
-                  << std::endl;  // Если видео кончилось, возращает -1
-        //[0] значит, что для первой видеозаписи(getQueuePeopleAmount возращает
-        // вектор специально, задел на будущее под несколько записей)
-      }
-    }
-  });
-
+  auto rt = getRouter();
   // Создаем второй поток для сервера
-  std::thread server_thread([]() {
-    auto rt = getRouter();
+  std::thread server_thread([&rt]() {
     Server tst(*rt);
     // http://localhost:8080/getFromDb?start=20230501000000000&end=20230501235900000
     tst.run();
+  });
+
+  // Создаем первый поток для цикла while(true)
+  // TODO: Добавить db_handler внутрь video_processor чтобы через него
+  // взаимодействовать с бд. Пока костыльно прокидываю роутер
+  std::thread video_thread([&rt]() {
+    while (true) {
+      auto vid_processor =
+          getVideoProcessor("../static/video_examples/ex1.mp4", 2);
+
+      std::cout << "Reopen video..." << std::endl;
+      int amount = 0;
+      while ((amount = vid_processor->getQueuePeopleAmount()[0]) != -1) {
+        // std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << "Current people amount from videosource: " << amount
+                  << std::endl;  // Если видео кончилось, возращает -1
+        //[0] значит, что для первой видеозаписи(getQueuePeopleAmount возращает
+        // вектор специально, задел на будущее под несколько записей)
+        auto time_now = std::chrono::system_clock::now();
+        rt->db_handler_->insertEntry(amount, time_now, 1);
+      }
+    }
   });
 
   // Дожидаемся окончания работы потоков
