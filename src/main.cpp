2330b1da7f1f9b72df713668e4f2e129dbbc673b
TimofeyTst <yarik.mist@gmail.com>
2023-05-15 16:17:23 +0300

    Одновременно работает сервак и CV

--- src/main.cpp
+++ src/main.cpp
@@ -1,29 +1,39 @@
-#include <boost/beast.hpp>
 #include <iostream>
+#include <thread>
 
 #include "routes.h"
 #include "server.h"
 #include "video_processors.h"
 
 int main(int argc, char* argv[]) {
-  while (true) {
-    // auto vid_processor = getVideoProcessor(0);
-    auto vid_processor =
-        getVideoProcessor("../static/video_examples/ex1.mp4", 2);
+  // Создаем первый поток для цикла while(true)
+  std::thread video_thread([]() {
+    while (true) {
+      auto vid_processor =
+          getVideoProcessor("../static/video_examples/ex1.mp4", 0.2);
 
-    for (int i = 0; i < 28; ++i) {
-      std::cout << i << " step . Current people amount from videosource: "
-                << vid_processor->getQueuePeopleAmount()[0]
-                << std::endl;  // Если видео кончилось, возращает -1
-    }  //[0] значит, что для первой видеозаписи(getQueuePeopleAmount возращает
-       // вектор специально, задел на будущее под несколько записей)
-  }
+      for (int i = 0; i < 28; ++i) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
+        std::cout << i << " step. Current people amount from videosource: "
+                  << vid_processor->getQueuePeopleAmount()[0]
+                  << std::endl;  // Если видео кончилось, возращает -1
+        //[0] значит, что для первой видеозаписи(getQueuePeopleAmount возращает
+        // вектор специально, задел на будущее под несколько записей)
+      }
+    }
+  });
 
-  auto rt = getRouter();
-  Server tst(*rt);
+  // Создаем второй поток для сервера
+  std::thread server_thread([]() {
+    auto rt = getRouter();
+    Server tst(*rt);
+    // http://localhost:8080/getFromDb?start=20230501000000000&end=20230501235900000
+    tst.run();
+  });
 
-  // http://localhost:8080/getFromDb?start=20230501000000000&end=20230501235900000
-  tst.run();
+  // Дожидаемся окончания работы потоков
+  video_thread.join();
+  server_thread.join();
 
   return 0;
 }
