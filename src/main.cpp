1ecf45ec107727fda8cadc5774d6a49f4556a735
TimofeyTst <yarik.mist@gmail.com>
2023-05-26 16:58:21 +0300

    Замедление процессов, убираем вебку

--- src/main.cpp
+++ src/main.cpp
@@ -18,36 +18,33 @@ int main(int argc, char* argv[]) {
   });
 
   // Создаем второй поток для цикла while(true)
-  // TODO: Добавить db_handler внутрь video_processor чтобы через него
-  // взаимодействовать с бд. Пока костыльно прокидываю роутер
-  std::thread video_thread([&rt]() {
-    SQLiteHandler db_handler("/app/db/db.db");
+  SQLiteHandler db_handler("/app/db/db.db");
+  std::thread video_thread([&db_handler]() {
     while (true) {
       auto vid_processor = getVideoProcessor();
 
       std::shared_ptr<FileVideo> fv1 = std::make_shared<FileVideo>(
-          "/app/static/video_examples/ex1.mp4", 0.5);
+          "/app/static/video_examples/ex1.mp4", 2);
       std::shared_ptr<FileVideo> fv2 = std::make_shared<FileVideo>(
-          "/app/static/video_examples/sample.mp4", 0.1);
-      std::shared_ptr<CamVideo> fv3 = std::make_shared<CamVideo>(0);
+          "/app/static/video_examples/sample.mp4", 2);
+      // std::shared_ptr<CamVideo> fv3 = std::make_shared<CamVideo>(0);
 
       vid_processor->pushBackVideoSource(
           fv1);  // Добавляем в видео_процессор источники для видео
       vid_processor->pushBackVideoSource(
           fv2, cv::Rect(10, 50, 500,
                         500));  // можно также указывать прямоугольник очереди
-      vid_processor->pushBackVideoSource(fv3, cv::Rect(10, 50, 500, 500));
+      // vid_processor->pushBackVideoSource(fv3, cv::Rect(10, 50, 500, 500));
       // vid_processor->setVisualizeVidSourceIndex(1);
-
 
       std::cout << "Reopen video..." << std::endl;
       std::vector<int> people_amounts = vid_processor->getQueuePeopleAmount();
-      while (((people_amounts[0]) != -1) || (people_amounts[1] != -1)) {
-        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+      while (((people_amounts[0]) != -1) && (people_amounts[1] != -1)) {
+        std::this_thread::sleep_for(std::chrono::seconds(2));
         std::cout << "Current people amount from first videosource: "
                   << people_amounts[0]
                   << ". From second videosource: " << people_amounts[1]
-                  << ". From third: " << people_amounts[2]
+                  // << ". From third: " << people_amounts[2]
                   << std::endl;  // Если видео кончилось, возращает -1
 
         auto time_now = std::chrono::system_clock::now();
