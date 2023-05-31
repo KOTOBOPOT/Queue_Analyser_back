b76bf90c4bbde3a76ae7a1b12d5ff8078c4e12e3
TimofeyTst <yarik.mist@gmail.com>
2023-05-31 15:27:28 +0300

    Максимальная гибкость в визуализации и веб-камеры

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <thread>
 
 #include "cam_video.h"
 #include "const.h"
@@ -9,28 +10,37 @@
 
 int main(int argc, char* argv[]) {
   SQLiteHandler db_handler(DOC_ROOT + "/db/db.db");
+  int pause = 10;
   while (true) {
     auto vid_processor = getVideoProcessor(DOC_ROOT + "/static/model");
 
     auto fv1 = std::make_shared<FileVideo>(
-        DOC_ROOT + "/static/video_examples/ex1.mp4", 0.5);
+        DOC_ROOT + "/static/video_examples/ex1.mp4", 1);
     auto fv2 = std::make_shared<FileVideo>(
-        DOC_ROOT + "/static/video_examples/sample.mp4", 0.1);
-    auto fv3 = std::make_shared<CamVideo>(0);
+        DOC_ROOT + "/static/video_examples/sample.mp4", 1);
 
     vid_processor->pushBackVideoSource(fv1);
     vid_processor->pushBackVideoSource(fv2, cv::Rect(10, 50, 500, 500));
-    vid_processor->pushBackVideoSource(fv3);
     
     if (argc > 1) {
-      int visualizeIndex = std::stoi(argv[1]);
-      vid_processor->setVisualizeVidSourceIndex(visualizeIndex);
+      if (std::string(argv[1]) != "-v") {
+        int visualizeIndex = std::stoi(argv[1]);
+        vid_processor->setVisualizeVidSourceIndex(visualizeIndex);
+        pause = 0;
+      } else {
+        auto fv3 = std::make_shared<CamVideo>(0);
+        vid_processor->pushBackVideoSource(fv3);
+        if (argc > 2) {
+          int visualizeIndex = std::stoi(argv[2]);
+          vid_processor->setVisualizeVidSourceIndex(visualizeIndex);
+          pause = 0;
+        }
+      }
     }
 
     std::cout << "Reopen video..." << std::endl;
     std::vector<int> people_amounts = vid_processor->getQueuePeopleAmount();
     while (((people_amounts[0]) != -1) && (people_amounts[1] != -1)) {
-      // std::this_thread::sleep_for(std::chrono::milliseconds(100));
       std::cout << "Current people amounts: ";
       for (int i = 0; i < people_amounts.size(); ++i) {
         std::cout << i << ": " << people_amounts[i] << ", ";
@@ -44,6 +54,7 @@ int main(int argc, char* argv[]) {
       }
 
       people_amounts = vid_processor->getQueuePeopleAmount();
+      std::this_thread::sleep_for(std::chrono::seconds(pause));
     }
   }
 
