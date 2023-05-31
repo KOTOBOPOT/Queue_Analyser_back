0d112fb2a903c6cdae2fd42721b17be221c08803
TimofeyTst <yarik.mist@gmail.com>
2023-05-31 09:27:52 +0300

    Добавляем константы

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,33 @@
 #include "server.h"
 #include "video_processors.h"
 
+const std::string DOC_ROOT = "/app";
+
 int main(int argc, char* argv[]) {
   auto rt = getRouter();
   // Создаем первый поток для сервера
   std::thread server_thread([&rt]() {
-    Server tst(*rt, "0.0.0.0");
+    Server tst(*rt, "0.0.0.0", 8080, DOC_ROOT + "src/views");
     // http://localhost:8080/getFromDb?start=20230501000000000&end=20230501235900000
     tst.run();
   });
 
   // Создаем второй поток для цикла while(true)
-  SQLiteHandler db_handler("/app/db/db.db");
-  std::thread video_thread([&db_handler]() {
+  // TODO: Добавить db_handler внутрь video_processor чтобы через него
+  // взаимодействовать с бд. Пока костыльно прокидываю роутер
+  std::thread video_thread([&rt]() {
+    SQLiteHandler db_handler(DOC_ROOT + "/db/db.db");
     while (true) {
+      // auto vid_processor =
+      //     getVideoProcessor("../static/video_examples/ex1.mp4", 0.1);
+
       auto vid_processor = getVideoProcessor();
 
-      std::shared_ptr<FileVideo> fv1 =
-          std::make_shared<FileVideo>("/app/static/video_examples/ex1.mp4", 2);
-      std::shared_ptr<FileVideo> fv2 = std::make_shared<FileVideo>(
-          "/app/static/video_examples/sample.mp4", 2);
-      std::shared_ptr<CamVideo> fv3 = std::make_shared<CamVideo>(0);
+      auto fv1 = std::make_shared<FileVideo>(
+          DOC_ROOT + "/static/video_examples/ex1.mp4", 0.5);
+      auto fv2 = std::make_shared<FileVideo>(
+          DOC_ROOT + "/static/video_examples/sample.mp4", 0.1);
+      auto fv3 = std::make_shared<CamVideo>(0);
 
       vid_processor->pushBackVideoSource(
           fv1);  // Добавляем в видео_процессор источники для видео
@@ -35,17 +42,22 @@ int main(int argc, char* argv[]) {
           fv2, cv::Rect(10, 50, 500,
                         500));  // можно также указывать прямоугольник очереди
       vid_processor->pushBackVideoSource(fv3, cv::Rect(10, 50, 500, 500));
-      // vid_processor->setVisualizeVidSourceIndex(1);
+
+      //Убрал визулизацию
+      //vid_processor->setVisualizeVidSourceIndex(
+      //   1);  // какое видео выводим. Указан индекс 2 - индекс в соответствии с
+               // pushBackVideoSource().  В данном случае выводится видео с веб
+               // камеры ноутбука
 
       std::cout << "Reopen video..." << std::endl;
       std::vector<int> people_amounts = vid_processor->getQueuePeopleAmount();
       while (((people_amounts[0]) != -1) && (people_amounts[1] != -1)) {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
-        std::cout << "Current people amounts: ";
-        for (int i = 0; i < people_amounts.size(); ++i) {
-          std::cout << i << ": " << people_amounts[i] << ", ";
-        };
-        std::cout << std::endl;
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
+        std::cout << "Current people amount from first videosource: "
+                  << people_amounts[0]
+                  << ". From second videosource: " << people_amounts[1]
+                  << ". From third: " << people_amounts[2]
+                  << std::endl;  // Если видео кончилось, возращает -1
 
         auto time_now = std::chrono::system_clock::now();
 
